73a4843d49558cc01c7af21c1beb1a5a18e24667
Peter TB Brett <peter.brett@livecode.com>
2014-11-19 17:24:32 +0000

    [[feature-stackarr]] Draft stack array manipulation API.

---
+++ engine/src/stackarr.cpp
@@ -0,0 +1,44 @@
+/*                                                                   -*- c++ -*-
+Copyright (C) 2003-2014 Runtime Revolution Ltd.
+
+This file is part of LiveCode.
+
+LiveCode is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License v3 as published by the Free
+Software Foundation.
+
+LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+
+#include "prefix.h"
+
+#include "globdefs.h"
+#include "filedefs.h"
+#include "objdefs.h"
+#include "parsedef.h"
+#include "mcio.h"
+#include "sysdefs.h"
+
+#include "stackarr.h"
+
+/* ================================================================
+ * High-level entry points
+ * ================================================================ */
+
+/* ----------------------------------------------------------------
+ * [Public] Initialisation
+ * ---------------------------------------------------------------- */
+
+void
+MCStackarrInitialize (void)
+{
+	static bool s_once = false;
+	if (s_once) return;
+
+	s_once = true;
+}
---
+++ engine/src/stackarr.h
@@ -0,0 +1,45 @@
+/*                                                                   -*- c++ -*-
+Copyright (C) 2003-2014 Runtime Revolution Ltd.
+
+This file is part of LiveCode.
+
+LiveCode is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License v3 as published by the Free
+Software Foundation.
+
+LiveCode is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with LiveCode.  If not see <http://www.gnu.org/licenses/>.  */
+
+#ifndef _MC_STACKARR_H_
+#define _MC_STACKARR_H_
+
+/* Stack arrays are a serialized representation of a stack.  They come
+ * in two forms:
+ *
+ * 1. Typed stack arrays have all values in their original type.
+ *
+ * 2. Detyped stack arrays have all value converted to a (type name,
+ *    base value) pair.
+ */
+
+/* ----------------------------------------------------------------
+ * High-level stack array functions.
+ * ---------------------------------------------------------------- */
+
+/* Initialize stack array support */
+void MCStackarrInitialize (void);
+
+bool MCStackarrCaptureStack (MCStack *stack, MCArrayRef & r_typed_state);
+
+bool MCStackarrConstructStack (MCArrayRef p_typed_state, MCStack *& r_stack);
+
+bool MCStackarrMakeTyped (MCArrayRef p_detyped_state, MCArrayRef & r_typed_state);
+
+bool MCStackarrMakeDetyped (MCArrayRef p_typed_state, MCArrayRef & r_detyped_state);
+
+#endif /* !_MC_STACKARR_H_ */
